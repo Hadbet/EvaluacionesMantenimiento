291000eee8bf3d5bda9564c197ef28701b3924a9
hadbet <hadbet.altamirano@grammer.com>
2025-02-04 14:43:54 -0600

    Cambios de values

--- historico.php
+++ historico.php
@@ -168,6 +168,66 @@
             });
         }
     });
+
+
+    $.ajax({
+        url: 'https://grammermx.com/Mantenimiento/Evaluaciones/dao/daoConsultaCuestionarioUno.php?idCuestionario=2', // Reemplaza esto con la URL de tus datos
+        dataType: 'json',
+        success: function(data) {
+            var table = $('#dataTable-2').DataTable({
+                data: data.data,
+                columns: [
+                    { data: 'IdRegistroEvaluacion' },
+                    { data: 'Nomina' },
+                    { data: 'Nombre' },
+                    { data: 'FechaRegistro' },
+                    { data: 'Pregunta' },
+                    { data: 'Respuesta' }
+                ],
+                autoWidth: true,
+                "lengthMenu": [
+                    [16, 32, 64, -1],
+                    [16, 32, 64, "All"]
+                ],
+                dom: 'Bfrtip',
+                buttons: [
+                    {
+                        extend: 'copy',
+                        className: 'btn btn-sm copyButton'
+                    },
+                    {
+                        extend: 'csv',
+                        className: 'btn btn-sm csvButton'
+                    },
+                    {
+                        extend: 'excel',
+                        className: 'btn btn-sm excelButton'
+                    },
+                    {
+                        extend: 'pdf',
+                        className: 'btn btn-sm pdfButton'
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn btn-sm printButton'
+                    }
+                ],
+                initComplete: function () {
+                    this.api().columns().every( function () {
+                        var column = this;
+                        var input = document.createElement("input");
+                        input.className = 'form-control form-control-sm';
+                        $(input).appendTo($(column.footer()).empty())
+                            .on('keyup change clear', function () {
+                                if (column.search() !== this.value) {
+                                    column.search(this.value).draw();
+                                }
+                            });
+                    });
+                }
+            });
+        }
+    });
 </script>
 </body>
 </html>
